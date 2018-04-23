1856b20e12e7d547d8a80da0a9ad6726d91098a0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MyComboBoxEdit/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+
+
+namespace MyComboBoxEdit
+{
+    public partial class FormMain : Form
+    {
+        private MyPopupHelper popupHelper;
+
+
+        public FormMain()
+        {
+            InitializeComponent();
+            popupHelper = new MyPopupHelper(panelControl1);   
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to adjust popup window's location of popup editor in scrollable container
+
+
+<p>This example illustrates how to adjust popup window's location of popup editor in scrollable container.</p>
+
+<br/>
+
+
